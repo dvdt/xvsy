1b11d59606a9e7edf67ac89a15f0ec9bb4d3bf61
David Tsao <davidtsao@gmail.com>
2015-06-30 13:53:03 -0500

    got figwheel working with google-http-client-jackson2

--- project.clj
+++ project.clj
@@ -4,6 +4,7 @@
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0-RC2"]
+                 [org.clojure/clojurescript "0.0-3211"]
                  [org.clojure/algo.generic "0.1.2"]
                  [org.clojure/math.combinatorics "0.0.8"]
                  [org.clojure/core.incubator "0.1.3"]
@@ -25,22 +26,27 @@
                  [korma "0.4.2"]
                  [postgresql "9.3-1102.jdbc41"]
                  [com.h2database/h2 "1.4.187"]
-                 [com.google.api-client/google-api-client "1.20.0"]
-                 [com.google.apis/google-api-services-bigquery "v2-rev217-1.20.0"]
-                 [com.google.http-client/google-http-client-jackson2 "1.20.0"]
                  [prismatic/schema "0.4.3"]
                  [org.clojars.mikejs/ring-gzip-middleware "0.1.0-SNAPSHOT"]
-                 [clj-time "0.9.0"]]
+                 [clj-time "0.9.0"]
+                 [reagent "0.5.0"]
+                 ;; include pomegranate above the google-http-client-jackson2 to have it's
+                 ;; dependencies resolved first.
+                 [com.cemerick/pomegranate "0.3.0"]
+                 [com.google.api-client/google-api-client "1.20.0"]
+                 [com.google.apis/google-api-services-bigquery "v2-rev217-1.20.0"]
+                 [com.google.http-client/google-http-client-jackson2 "1.20.0"]]
+
   ;;:resource-paths ["resources/bqjdbc-1.4.jar"]
 
   :source-paths ["src/clj" "src/cljc"]
   :plugins [[lein-cljsbuild "1.0.6"]
-            [lein-figwheel "0.3.3"]]
+            [lein-figwheel "0.3.5"]]
   :clean-targets ^{:protect false} ["resources/public/js/compiled" "target"]
   :cljsbuild {:builds [{:id "dev"
-                        :source-paths ["src/cljs" "src/cljc"]
+                        :source-paths ["src/cljs" #_"src/cljc"]
 
-                        :figwheel {:on-jsload "xvsy.core/main"}
+                        :figwheel {:on-jsload "xvsy.client.core/main"}
 
                         :compiler {:main xvsy.client.core
                                    :output-to "resources/public/js/compiled/app.js"
@@ -49,19 +55,16 @@
                                    :source-map-timestamp true}}
 
                        {:id "min"
-                        :source-paths ["src/cljs" "src/cljs"]
+                        :source-paths ["src/cljs" #_"src/cljc"]
                         :compiler {:main xvsy.client.core
-                                   :output-to "resources/public/js/compiled/app.js"
+                                   :outputdi-to "resources/public/js/compiled/app.js"
                                    :optimizations :advanced
                                    :pretty-print false}}]}
   :profiles
-  {:dev {:source-paths ["dev"]
-         :dependencies [[javax.servlet/servlet-api "2.5"]
-                        [org.clojure/tools.namespace "0.2.10"]
-                        [ring-mock "0.1.5"]]}
+  {:dev {:dependencies []}
    :production {:aot :all}
    :uberjar {:aot :all}}
-  :main ^:skip-aot xvsy.handlers
+  ;; :main ^:skip-aot xvsy.handlers
   :uberjar-name "xvsy-standalone.jar"
   :min-lein-version "2.5.0"
   :repositories [["sonatype" {:url "http://oss.sonatype.org/content/repositories/releases"}]
